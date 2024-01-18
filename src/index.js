7082ddf6ce2f4a83be8b01ff8c223a79fb7e42c4
Kirstyn Canull <kirstyncanull@kirstyns-air.mynetworksettings.com>
2024-01-18 01:05:18 -0500

    completed first two core deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 // write your code here 
+fetch("http://localhost:3000/ramens")
+    .then(response => response.json())
+    .then(ramens => {
+        const ramenList = document.querySelector("#ramen-menu")
+        ramens.forEach(ramen => {
+            const img = document.createElement("img");
+            img.setAttribute('src', ramen.image);
+            ramenList.append(img);
+            img.addEventListener("click", () => {
+                const info = document.getElementById("#ramen-detail")
+                const infoImage = document.querySelector("#ramen-detail .detail-image");
+                infoImage.setAttribute('src', ramen.image);
+                const infoRamenName = ramen.name;
+                document.querySelector("#ramen-detail .name").textContent = infoRamenName;
+                const infoRestaurantName = ramen.restaurant;
+                document.querySelector("#ramen-detail .restaurant").textContent = infoRestaurantName;
+                const infoRating = ramen.rating;
+                document.getElementById("rating-display").textContent = infoRating;
+                const infoComment = ramen.comment;
+                document.getElementById("comment-display").textContent = infoComment;
+            })
+        })
+    }
+
+    );
+
+
+    // showButton.addEventListener("click", () => {
+    //     myDiv.style.display = "block";
+    //   });
+
+    // function displayInfo (ramenInfo){
+    //     const info = document.getElementById("#ramen-detail")
+    //     const ramenName = ramen.name;
+    //     document.getElementsByClassName("name").textContent = ramenName;
+    //     const restaurantName = ramen.restaurant;
+    //     document.getElementsByClassName("restaurant").textContent = restaurantName;
